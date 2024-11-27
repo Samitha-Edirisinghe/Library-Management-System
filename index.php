135f15550ee880f42df48ecbf6d2114db3dff6da
Samitha Randika Edirisinghe <114610125+Samitha-Edirisinghe@users.noreply.github.com>
2024-11-27 22:14:41 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,111 @@
+<?php include 'includes/session.php'; ?>
+<?php
+	$where = '';
+	if(isset($_GET['category'])){
+		$catid = $_GET['category'];
+		$where = 'WHERE category_id = '.$catid;
+	}
+?>
+<?php include 'includes/header.php'; ?>
+<body class="hold-transition skin-blue layout-top-nav">
+<div class="wrapper">
+
+	<?php include 'includes/navbar.php'; ?>
+	 
+	  <div class="content-wrapper"  style="background: url(images/bg.jpg); width:100%">
+	    <div class="container">
+
+	      <!-- Main content -->
+	      <section class="content">
+	        <div class="row">
+	        	<div class="col-sm-8 col-sm-offset-2">
+	        		<?php
+	        			if(isset($_SESSION['error'])){
+	        				echo "
+	        					<div class='alert alert-danger'>
+	        						".$_SESSION['error']."
+	        					</div>
+	        				";
+	        				unset($_SESSION['error']);
+	        			}
+	        		?>
+	        		<div class="box">
+	        			<div class="box-header with-border">
+	        				<div class="input-group">
+				                <input type="text" class="form-control input-lg" id="searchBox" placeholder="Search for ISBN, Title or Author">
+				                <span class="input-group-btn">
+				                    <button type="button" class="btn btn-primary btn-flat btn-lg"><i class="fa fa-search"></i> </button>
+				                </span>
+				            </div>
+	        			</div>
+	        			<div class="box-body">
+	        				<div class="input-group col-sm-5">
+				                <span class="input-group-addon">Category:</span>
+				                <select class="form-control" id="catlist">
+				                	<option value=0>ALL</option>
+				                	<?php
+				                		$sql = "SELECT * FROM category";
+				                		$query = $conn->query($sql);
+				                		while($catrow = $query->fetch_assoc()){
+				                			$selected = ($catid == $catrow['id']) ? " selected" : "";
+				                			echo "
+				                				<option value='".$catrow['id']."' ".$selected.">".$catrow['name']."</option>
+				                			";
+				                		}
+				                	?>
+				                </select>
+				             </div>
+	        				<table class="table table-bordered table-striped" id="booklist">
+			        			<thead>
+			        				<th>ISBN</th>
+			        				<th>Title</th>
+			        				<th>Author</th>
+			        				<th>Status</th>
+			        			</thead>
+			        			<tbody>
+			        			<?php
+			        				$sql = "SELECT * FROM books $where";
+			        				$query = $conn->query($sql);
+			        				while($row = $query->fetch_assoc()){
+			        					$status = ($row['status'] == 0) ? '<span class="label label-success">available</span>' : '<span class="label label-danger">not available</span>';
+			        					echo "
+			        						<tr>
+			        							
+			        							<td>".$row['isbn']."</td>
+			        							<td>".$row['title']."</td>
+			        							<td>".$row['author']."</td>
+			        							<td>".$status."</td>
+			        						</tr>
+			        					";
+			        				}
+			        			?>
+			        			</tbody>
+			        		</table>
+	        			</div>
+	        		</div>
+	        	</div>
+	        </div>
+	      </section>
+	     
+	    </div>
+	  </div>
+  
+  	<?php include 'includes/footer.php'; ?>
+</div>
+
+<?php include 'includes/scripts.php'; ?>
+<script>
+$(function(){
+	$('#catlist').on('change', function(){
+		if($(this).val() == 0){
+			window.location = 'index.php';
+		}
+		else{
+			window.location = 'index.php?category='+$(this).val();
+		}
+		
+	});
+});
+</script>
+</body>
+</html>
